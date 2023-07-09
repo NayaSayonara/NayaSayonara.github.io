1b800c4f94baa85c9199b188896ba2f276240c24
Anastasiia <113624371+NayaSayonara@users.noreply.github.com>
2023-07-09 15:30:23 +0200

    Update selectpaymentmethod.js

---
+++ js/selectpaymentmethod.js
@@ -0,0 +1,4 @@
+document.getElementById("linkCreditCard").addEventListener("click", function(){
+    document.getElementById("selectPaymentMethod").hidden = true;
+    document.getElementById("creditCard").hidden = false;
+}, false);
